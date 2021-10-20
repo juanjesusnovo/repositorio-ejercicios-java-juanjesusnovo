a23e01351584d0fe22ec867607e3746b4e96a8d6
juanjesusnovo <jjnovo.alumno@iesrafaelalberti.es>
2021-10-20 14:09:00 +0200

    Ejercicio56

--- src/Relacion52/Ejercicio56.java
+++ src/Relacion52/Ejercicio56.java
@@ -1,4 +1,45 @@
 package Relacion52;
 
+import java.security.cert.CertPathValidatorException;
+import java.util.Scanner;
+
 public class Ejercicio56 {
+
+    public static void main(String[] args){
+        Ejercicio56 op = new Ejercicio56();
+        op.comprobar();
+        op.Calc_area();
+        op.Calc_perimetro();
+        System.out.println("Area: "+ op.area);
+        System.out.println("Perimetro: " + op.perimetro);
+    }
+
+    Scanner datos = new Scanner(System.in);
+
+    float area, perimetro, base, altura;
+
+    public void comprobar(){
+        System.out.print("Introduce la base y la altura: ");
+        while (!datos.hasNextFloat()){
+            System.out.println("Algo no ha funcionao");
+            datos.nextLine();
+        }
+        base = datos.nextFloat();
+        datos.nextLine();
+        while (!datos.hasNextFloat()){
+            System.out.println("Algo no ha funcionao");
+            datos.nextLine();
+        }
+        altura = datos.nextFloat();
+    }
+
+
+    public float Calc_area(){
+        area = base * altura;
+        return area;
+    }
+    public float Calc_perimetro(){
+        perimetro = (base * 2) + (altura * 2);
+        return perimetro;
+    }
 }
